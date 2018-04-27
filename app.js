a4b2c3b70d8076e8d99f935c5ac476f8fc470d09
MenciusKeal <1343478979@qq.com>
2018-04-27 14:52:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
- App({
 
+
+App({
+  // 全局数据
   globalData: {
-
+   
   },
+  // 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
   onLaunch: function () {
-    this.login();
-  },
-  onShow: function (options) {
-    
+    this.login()
   },
   // 登录
   login: function () {
-    // https://juejin.im/post/5ac9b72cf265da23906c486a    
+    var that = this;
     var skey = wx.getStorageInfoSync('skey');
     wx.checkSession({
       success: function () {
@@ -22,7 +22,6 @@
         wx.login({
           success: function (res) {
             if (res.code) {
-              console.log(res)
               wx.request({
                 url: 'http://mps.essocial.com.cn/api/login/login',
                 data: {
@@ -31,7 +30,14 @@
                 },
                 method: 'POST',
                 success: function (res) {
+                  console.log(res)
+                  
                   wx.setStorageSync('skey', res.data.data.skey)
+
+                  that.globalData.customer_id = res.data.data.customer_id;
+                  that.globalData.oId = res.data.data.openid;
+                  that.globalData.skey = res.data.data.skey
+                  
                   wx.showModal({
                     title: '提 示',
                     content: '登录成功！',
