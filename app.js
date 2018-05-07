82efa2773608865453317640938dc7b4a2a68294
MenciusKeal <1343478979@qq.com>
2018-05-07 16:44:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-
+ 
 
 App({
   // 全局数据
   globalData: {
-   
+
   },
   // 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
   onLaunch: function () {
@@ -12,10 +12,14 @@ App({
   // 登录
   login: function () {
     var that = this;
-    var skey = wx.getStorageInfoSync('skey');
+    var skey = wx.getStorageSync('skey')
     wx.checkSession({
-      success: function () {
-        console.log('skey未过期');
+      success: function (res) {
+        // console.log('skey未过期');
+        // wx.showModal({           
+        //   content: '欢迎回来',
+        //   showCancel:false
+        // })
       },
       fail: function () {
         console.log('skey已失效');
@@ -31,15 +35,16 @@ App({
                 method: 'POST',
                 success: function (res) {
                   console.log(res)
-                  
+                  // 登录有效期skey
                   wx.setStorageSync('skey', res.data.data.skey)
+                  // 用户id
+                  wx.setStorageSync('c_id', res.data.data.customer_id)
 
-                  that.globalData.customer_id = res.data.data.customer_id;
-                  that.globalData.oId = res.data.data.openid;
-                  that.globalData.skey = res.data.data.skey
-                  
+                  // openid
+                  // wx.setStorageSync('open_id', res.data.data.openid)
+
                   wx.showModal({
-                    title: '提 示',
+                    title: '提示',
                     content: '登录成功！',
                     showCancel: false
                   })
@@ -50,6 +55,8 @@ App({
         })
       }
     })
+
+
   }
 
 })
