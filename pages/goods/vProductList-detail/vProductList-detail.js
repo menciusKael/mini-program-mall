0f33470660f6071f6d7b80b1ab32dd0dabaa2119
MenciusKael <1343478979@qq.com>
2018-05-29 10:16:09 +0800

    增加分享商品海报

--- pages/goods/vProductList-detail/vProductList-detail.js
+++ pages/goods/vProductList-detail/vProductList-detail.js
@@ -1,3 +1,5 @@
+var prom = require('../../../utils/promise.js');
+var WxParse = require('../../../wxParse/wxParse.js');
 Page({
   data: {
     currentSelectd: 1,
@@ -12,8 +14,16 @@ Page({
     buyNumMax: 0,
     shopNum: 0,
     shopCarInfo: {},
+    buyNowInfo: {},
     shopType: 'addShopCar',
-    onShare: false
+    onShare: false,
+    allComment: true,
+    collected: 0,
+    goodsComment: [], // 商品评论
+    // 海报
+    qrCodePath: '',
+    goodsPicPath: '',
+    posterShow: false
   },
 
   onLoad: function (options) {
@@ -30,23 +40,28 @@ Page({
     })
 
     let id = options.id;
-
+    var c_id = wx.getStorageSync('c_id')
     // 商品详情
     wx.request({
       url: 'http://mps.essocial.com.cn/api/product/getProductDetail',
       method: 'POST',
       data: {
-        product_id: id
+        product_id: id,
+        customer_id: c_id
       },
       success: function (res) {
         var goodsDetail = res.data.data;
         console.log(res);
+        var description = goodsDetail.description;
         that.setData({
           goodsDetail,
           price: goodsDetail.basicInfo.price,
           buyNumMax: goodsDetail.basicInfo.stock,
-          buyNumber: goodsDetail.basicInfo.stock ? 1 : 0
+          buyNumber: goodsDetail.basicInfo.stock ? 1 : 0,
+          collected: goodsDetail.basicInfo.collected
         })
+
+        WxParse.wxParse('description', 'html', description, that, 5);
         // 规格
         // if (goodsDetail.option) {
         //   var optionsTemp = '';
@@ -59,6 +74,8 @@ Page({
         // })
       },
     })
+
+    var scene = decodeURIComponent(options.scene)
   },
 
   // 规格选择
@@ -141,6 +158,17 @@ Page({
     return shopCarInfo;
 
   },
+  // 组件立即购买信息
+  buildBuyNowInfo() {
+    var buyNowItem = {}
+    buyNowItem.id = this.data.goodsDetail.basicInfo.product_id;
+    buyNowItem.price = this.data.goodsDetail.basicInfo.price;
+    buyNowItem.name = this.data.goodsDetail.basicInfo.name;
+    buyNowItem.pic = this.data.goodsDetail.pictures[0];
+    buyNowItem.buyNumber = this.data.buyNumber;
+    buyNowItem.size = this.data.optionSelect;
+    return buyNowItem;
+  },
 
   // 规格选项排序
   sizeKeySort(obj) {
@@ -192,7 +220,38 @@ Page({
     })
 
   },
+  // 立即购买
+  buyNow() {
+    if (this.data.goodsDetail.option.length !== Object.keys(this.data.optionSelect).length) {
+      wx.showModal({
+        title: '提 示',
+        content: '请选择规格！',
+        showCancel: false
+      })
+      return;
+    }
 
+    if (!this.data.price) {
+      wx.showModal({
+        title: '提 示',
+        content: '暂无相应规格产品',
+        showCancel: false
+      })
+      return;
+    }
+    var buyNowInfo = this.buildBuyNowInfo()
+    this.setData({
+      buyNowInfo
+    })
+    wx.setStorage({
+      key: 'buyNowInfo',
+      data: buyNowInfo
+    })
+    this.closepropPopup();
+    wx.navigateTo({
+      url: '/pages/order-confirm/order-confirm?shopType=buyNow',
+    })
+  },
   // 商品轮播图放大查看
   previewImage(e) {
     var current = e.target.dataset.src;
@@ -232,12 +291,30 @@ Page({
     })
   },
 
-  // 商品详情与评价切换
+  // 商品详情,参数,评价
   onchangeItem(e) {
+    var that = this;
+    var productId = that.data.goodsDetail.basicInfo.product_id;
     let id = e.currentTarget.dataset.id
-    this.setData({
+    that.setData({
       currentSelectd: id
     })
+    if (id == 3) {
+      wx.request({
+        url: 'http://mps.essocial.com.cn/api/product/getProductCommentList',
+        data: {
+          product_id: productId
+        },
+        method: 'POST',
+        success: function (res) {
+          let allCommentData = res.data.data;
+          console.log(allCommentData)
+          that.setData({
+            goodsComment: allCommentData
+          })
+        }
+      })
+    }
   },
 
   // 规格弹窗 购买数量加
@@ -284,27 +361,57 @@ Page({
 
   // 商品收藏/取消收藏
   onCollectTap(e) {
-    if (!this.data.collected) {
-      this.setData({
-        collected: true
-      })
-      wx.showToast({
-        title: '收藏成功',
-        success: function () {
-          wx.setStorageSync("collected", "yes")
-        }
-      })
-    } else {
-      this.setData({
-        collected: false
-      })
-      wx.showToast({
-        title: '取消收藏成功',
-        success: function () {
-          wx.removeStorageSync("collected");
+    var that = this;
+    var c_id = wx.getStorageSync('c_id')
+    var productId = that.data.goodsDetail.basicInfo.product_id;
+    var collected = that.data.collected;
+    if (!collected) {
+      wx.request({
+        url: 'http://mps.essocial.com.cn/api/product_collected/addProductCollected',
+        data: {
+          program_type: 3,
+          product_id: productId,
+          customer_id: c_id,
+          program_id: 2
+        },
+        method: 'POST',
+        success: function (res) {
+          if (res.data.status) {
+            that.setData({
+              collected: 1
+            })
+          }
+          wx.showToast({
+            title: '收藏成功',
+            icon: 'success',
+            mask: true
+          })
         }
       })
     }
+    if (collected) {
+      wx.request({
+        url: 'http://mps.essocial.com.cn/api/product_collected/deleteProductCollected',
+        data: {
+          customer_id: c_id,
+          product_id: productId
+        },
+        method: 'POST',
+        success: function (res) {
+          if (res.data.status) {
+            that.setData({
+              collected: 0
+            })
+          }
+          wx.showToast({
+            title: '取消收藏成功',
+            icon: 'success',
+            mask: true
+          })
+        }
+      })
+    }
+
   },
   // 分享  
   onShareGood() {
@@ -312,7 +419,7 @@ Page({
       onShare: true
     })
   },
-  // 取消收藏
+  // 取消 
   oncCncelShare() {
     this.setData({
       onShare: false
@@ -331,6 +438,194 @@ Page({
         })
       }
     }
-  }
+  },
+  changeComType() {
+    this.setData({
+      allComment: !this.data.allComment
+    })
+  },
+
+  sharePoster() {
+    var that = this;
+    var goodsDetail = that.data.goodsDetail;
+    wx.getImageInfo({
+      src: goodsDetail.pictures[0],//服务器返回的带参数的小程序码地址
+      success: function (res) {
+        //res.path是网络图片的本地地址
+        that.setData({
+          goodsPicPath: res.path,
+          // posterShow :true
+        })
+      },
+      fail: function (res) {
+        //失败回调
+      }
+    });
+    wx.request({
+      url: 'http://mps.essocial.com.cn/api/product/getProductQrCode',
+      data: {
+        product_id: "1",
+        page_path: "pages/decor/article/article",
+        program_id: "2"
+      },
+      header: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      method: "POST",
+      success: function (res) {
+        wx.getImageInfo({
+          src: res.data.data,//服务器返回的带参数的小程序码地址
+          success: function (re) {
+            //re.path是网络图片的本地地址
+            that.drawSharePic(re.path);
+          },
+          fail: function (res) {
+            //失败回调
+          }
+        });
+      }
+    })
+  },
+
+  /**
+ * 绘制分享的图片
+ */
+  drawSharePic: function (qrCode) {
+    var that = this;
+    
+    var goodsDetail = that.data.goodsDetail;
+
+    //y方向的偏移量，因为是从上往下绘制的，所以y一直向下偏移，不断增大。
+    let yOffset = 10;
+    let goodsPicPath = this.data.goodsPicPath;
+    let qrCodePath = qrCode;
+    console.log(goodsPicPath, qrCodePath);
+    // let qrCodePath = this.data.qrCodePath;
+    const goodsTitle = goodsDetail.basicInfo.name;
+    let goodsTitleArray = [];
+    //为了防止标题过长，分割字符串,每行18个
+    for (let i = 0; i < goodsTitle.length / 18; i++) {
+      if (i > 2) {
+        break;
+      }
+      goodsTitleArray.push(goodsTitle.substr(i * 18, 20));
+    }
+    const price = goodsDetail.basicInfo.price;
+    const marketPrice = "123";
+    const name = "小程序名称";
+    const title1 = '您的好友邀请您一起分享精品好货';
+    const title2 = '立即打开看看吧';
+    const codeText = '长按识别小程序码';
+    const imgWidth = 280;
+    const imgHeight = 390;
+
+    const canvasCtx = wx.createCanvasContext('shareCanvas');
+    //绘制背景
+    canvasCtx.setFillStyle('white');
+    canvasCtx.fillRect(0, 0, imgWidth, imgHeight);
+    //绘制分享的标题文字
+    canvasCtx.setFontSize(14);
+    canvasCtx.setFillStyle('#333333');
+    canvasCtx.setTextAlign('center');
+    canvasCtx.fillText(title1, 144, 16);
+    //绘制分享的第二行标题文字
+    canvasCtx.fillText(title2, 144, 30);
+    //绘制商品图片
+    canvasCtx.drawImage(goodsPicPath, 0, 38, 280, 180);
+    //绘制商品标题
+    yOffset = 240;
+    goodsTitleArray.forEach(function (value) {
+      canvasCtx.setFontSize(14);
+      canvasCtx.setFillStyle('#333333');
+      canvasCtx.setTextAlign('left');
+      canvasCtx.fillText(value, 2, yOffset);
+      yOffset += 16;
+    });
+    //绘制价格
+    yOffset += 8;
+    canvasCtx.setFontSize(14);
+    canvasCtx.setFillStyle('#f9555c');
+    canvasCtx.setTextAlign('left');
+    canvasCtx.fillText('￥', 2, yOffset);
+    canvasCtx.setFontSize(16);
+    canvasCtx.setFillStyle('#f9555c');
+    canvasCtx.setTextAlign('left');
+    canvasCtx.fillText(price, 12, yOffset);
+    //绘制原价
+    const xOffset = (price.length / 2 + 1) * 18 + 18;
+    canvasCtx.setFontSize(14);
+    canvasCtx.setFillStyle('#999999');
+    canvasCtx.setTextAlign('left');
+    canvasCtx.fillText('原价:¥' + marketPrice, xOffset, yOffset);
+    //绘制原价的删除线
+    canvasCtx.setLineWidth(1);
+    canvasCtx.moveTo(xOffset, yOffset - 6);
+    canvasCtx.lineTo(xOffset + (3 + marketPrice.toString().length / 2) * 14, yOffset - 6);
+    canvasCtx.setStrokeStyle('#999999');
+    canvasCtx.stroke();
+    //绘制分割线
+    canvasCtx.setLineWidth(0.5);
+    canvasCtx.moveTo(0, yOffset + 10);
+    canvasCtx.lineTo(280, yOffset + 10);
+    canvasCtx.setStrokeStyle('#dddddd');
+    canvasCtx.stroke();
+    //绘制最底部文字
+    canvasCtx.setFontSize(14);
+    canvasCtx.setFillStyle('#333333');
+    // canvasCtx.setTextAlign('center');
+    canvasCtx.fillText(name, 120, 320);
+
+    canvasCtx.setFontSize(14);
+    canvasCtx.setFillStyle('#333333');
+    // canvasCtx.setTextAlign('center');
+    canvasCtx.fillText(codeText, 120, 340);
+    //绘制二维码
+    canvasCtx.drawImage(qrCodePath, 20, 300, 80, 80);
+    canvasCtx.draw();
+
+    //绘制之后加一个延时去生成图片，如果直接生成可能没有绘制完成，导出图片会有问题。
+    setTimeout(function () {
+      wx.canvasToTempFilePath({
+        x: 0,
+        y: 0,
+        width: imgWidth,
+        height: imgHeight,
+        destWidth: imgWidth,
+        destHeight: imgHeight,
+        canvasId: 'shareCanvas',
+        success: function (res) {
+          that.setData({
+            shareImage: res.tempFilePath,
+            showSharePic: true,
+
+          })
+          wx.hideLoading();
+        },
+        fail: function (res) {
+          console.log(res)
+
+        }
+      })
+    }, 2000);
+    that.setData({
+      posterShow: true
+    })
+  },
+  saveImage() {
+    const wxCanvasToTempFilePath = prom.promisify(wx.canvasToTempFilePath)
+    const wxSaveImageToPhotosAlbum = prom.promisify(wx.saveImageToPhotosAlbum)
+
+    wxCanvasToTempFilePath({
+      canvasId: 'shareCanvas'
+    }, this).then(res => {
+      return wxSaveImageToPhotosAlbum({
+        filePath: res.tempFilePath
+      })
+    }).then(res => {
+      wx.showToast({
+        title: '已保存到相册'
+      })
+    })
+  },
 
 })
