75f9d996b24fbf756c2d91b8ab51ecff64a2f7ea
MenciusKael <1343478979@qq.com>
2018-07-03 17:42:08 +0800

    Update sort.js

--- pages/sort/sort.js
+++ pages/sort/sort.js
@@ -1,73 +1,31 @@
-// pages/user/user.js
+var app = getApp()
+import { getMultiProductGroupList } from '../../utils/util'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    selected: true,
-    currentTab: 1
+    currentTab: 0
   },
-
-  choose(evevt) {
-    let id = evevt.currentTarget.dataset.id;
-    this.setData({
-      currentTab: id
+  onShow: function (options) {
+    var that = this;
+    getMultiProductGroupList(app.globalData.program_id).then(res => {
+      console.log(res)
+      let resData = res.data.data;
+      let first = resData[0].product_group_id
+      var tab = wx.getStorageSync('selectTab');console.log(tab)
+      that.setData({
+        currentTab: tab ? tab : first,
+        sortData: resData
+      })
     })
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-      
+  choose(e) {
+    this.setData({
+      currentTab: e.currentTarget.dataset.index
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  toProductDetail(e) {
+    let id = e.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: '/pages/goods/goods?id=' + id,
+    })
   }
 })
